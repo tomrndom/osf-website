f6678f38a348bc9acbfbc44ed2d84eb34e98f8bb
Manuel <manuel@cvam.com.ar>
2019-04-26 16:36:59 -0300

    Carousel: removed Bootstrap dependency Now uses standalone lib: github.com/mekkli/bootstrap-4.0-carousel-only

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -7,8 +7,7 @@ module.exports = {
     description: "The Home of Open Infrastructure",
     head: [["link", { rel: "icon", href: "/favicons/logo-openstack.ico" }],
             ["script", { src: "https://code.jquery.com/jquery-3.3.1.slim.min.js" }],
-            ["script", { src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" }],
-            ["script", { src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" }],
+            ["script", { src: "/js/carousel.min.js" }],
         ],    
     themeConfig: {
         nav: [
