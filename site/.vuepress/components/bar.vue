251848509797d275481af7a5f71a6b7d3453bf33
allisonprice <35505278+allisonprice@users.noreply.github.com>
2020-01-13 12:17:34 -0600

    Updating banner to promote Vancouver

--- site/.vuepress/components/bar.vue
+++ site/.vuepress/components/bar.vue
@@ -7,7 +7,7 @@
         </div> -->
         <!-- /.bar-logo -->
         <div class="bar-entry">
-          <p>The Open Infrastructure Summit is going to Shanghai! <a href="https://www.openstack.org/summit/shanghai-2019/" target="_blank" class="bar-link">Learn more <i class="right"></i> </a></p> 
+          <p>Come collaborate and participate in the OSF's new event: OpenDev + PTG, June 8-11 in Vancouver!<a href="https://www.openstack.org/summit/vancouver-2020/" target="_blank" class="bar-link">Learn more <i class="right"></i> </a></p> 
         </div>
         <!-- /.bar-entry -->
         
