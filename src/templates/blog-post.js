0125725f374b1b32ab7a1f7d0da333885ff9622f
Jimmy McArthur <jimmy@tipit.net>
2020-10-18 19:06:33 -0500

    Removing image tags from HTML meta tag render
    Signed-off-by: Jimmy McArthur <jimmy@tipit.net>

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -81,23 +81,17 @@ const BlogPost = ({ data }) => {
         helmet={
           <Helmet titleTemplate="%s | Blog">
             <title>{`${post.frontmatter.seo.title ? post.frontmatter.seo.title : post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.seo.description}`}
-            />
-            <meta name="image" content={`${withPrefix('/')}${post.frontmatter.seo.image.publicURL ? post.frontmatter.seo.image.publicURL : metadata.siteMetadata.image}`} />
+            <meta name="description" content={`${post.frontmatter.seo.description}`} />
             <meta property="og:type" content="article" />
             <meta property="og:title" content={`${post.frontmatter.seo.title ? post.frontmatter.seo.title : post.frontmatter.title}`} />
             <meta property="og:url" content={`${post.frontmatter.seo.url ? post.frontmatter.seo.url : metadata.siteMetadata.url}`} />
             <meta property="og:description" content={`${post.frontmatter.seo.description ? post.frontmatter.seo.description : metadata.siteMetadata.description}`} />
-            <meta property="og:image" content={`${withPrefix('/')}${post.frontmatter.seo.image.publicURL ? post.frontmatter.seo.image.publicURL : metadata.siteMetadata.image}`} />
             <meta property="article:published_time" content={`${post.frontmatter.date}`} />
             <meta property="article:author" content={`${post.frontmatter.author}`} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:creator" content={post.frontmatter.seo.twitterUsername ? post.frontmatter.seo.twitterUsername : metadata.siteMetadata.twitterUsername} />
             <meta name="twitter:title" content={`${post.frontmatter.seo.title ? post.frontmatter.seo.title : post.frontmatter.title}`} />
             <meta name="twitter:description" content={`${post.frontmatter.seo.description ? post.frontmatter.seo.description : metadata.siteMetadata.description}`} />
-            <meta name="twitter:image" content={`${withPrefix('/')}${post.frontmatter.seo.image.publicURL ? post.frontmatter.seo.image.publicURL : metadata.siteMetadata.image}`} />
           </Helmet>
         }
         title={post.frontmatter.title}
