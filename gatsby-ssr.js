209b3b409e5140c25f7fbc2fb90c05c8aaa51826
Tomás Castillo <tcastilloboireau@gmail.com>
2021-02-01 18:51:46 -0300

    Testing sort functions for og tags on SSR file
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,6 +9,38 @@ export const replaceRenderer = ({ wrapRootElement }) => {
     </SEO>
 }
 
+import Helmet from "react-helmet"
+
+export const onRenderBody = (
+    { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+    pluginOptions
+) => {
+    const helmet = Helmet.renderStatic()
+    setHtmlAttributes(helmet.htmlAttributes.toComponent())
+    setBodyAttributes(helmet.bodyAttributes.toComponent())
+    setHeadComponents([
+        helmet.title.toComponent(),
+        helmet.link.toComponent(),
+        helmet.meta.toComponent(),
+        helmet.noscript.toComponent(),
+        helmet.script.toComponent(),
+        helmet.style.toComponent(),
+    ])
+}
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+    const headComponents = getHeadComponents()
+    headComponents.sort((x, y) => {
+        if (x.props && x.props["data-react-helmet"]) {
+            return -1
+        } else if (y.props && y.props["data-react-helmet"]) {
+            return 1
+        }
+        return 0
+    })
+    replaceHeadComponents(headComponents)
+}
+
 import { JSDOM } from 'jsdom'
 import { Blob } from 'blob-polyfill';
 import { XMLHttpRequest } from 'xmlhttprequest';
