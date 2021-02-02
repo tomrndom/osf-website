7aee2855597ac38a581caf8fa8e5929da21a7483
Tomás Castillo <tcastilloboireau@gmail.com>
2021-02-02 09:15:49 -0300

    Fix issues with twitter card and OG tags
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,33 @@
 import ReduxWrapper from "./src/state/ReduxWrapper"
 // @see https://www.gatsbyjs.com/docs/adding-redux-store/
-export const wrapRootElement = ReduxWrapper
+export const wrapRootElement = ReduxWrapper;
+
+export const onPreRenderHTML = ({
+    getHeadComponents,
+    replaceHeadComponents,
+}) => {
+    const headComponents = getHeadComponents();
+    headComponents.forEach(head => {
+        if (head.props && head.props['data-react-helmet']) {
+            delete head.props['data-react-helmet'];
+        }
+    });
+    headComponents.sort((a, b) => {
+        if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+            return 0;
+        }
+
+        if (a.type === 'style') {
+            return 1;
+        } else if (b.type === 'style') {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    replaceHeadComponents(headComponents);
+};
 
 import { JSDOM } from 'jsdom'
 import { Blob } from 'blob-polyfill';
@@ -11,11 +38,11 @@ global.window = dom.window
 global.document = dom.window.document
 global.navigator = global.window.navigator
 
-global.window.matchMedia = function() {
+global.window.matchMedia = function () {
     return {
         matches: false,
-        addListener: function() {},
-        removeListener: function() {}
+        addListener: function () { },
+        removeListener: function () { }
     }
 }
 
